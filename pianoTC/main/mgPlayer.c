593a4a2943f23e5619a7765a8a38d48af777109c
Kromi <33200410+4weKromi@users.noreply.github.com>
2018-07-02 14:28:49 +0530

    Update mgPlayer.c

--- pianoTC/main/mgPlayer.c
+++ pianoTC/main/mgPlayer.c
@@ -1,5 +1,6 @@
 /*
   mgPlayer v0.1 by 4wekromi (march 2017)
+  -plays music.mg files
 */
 #include <stdio.h>
 #include <conio.h>
@@ -8,86 +9,72 @@
 char fnameA[]={"OdeToJoy-LudwigVanBeethoven.mg"};
 int pushToMem(float k,float d);
 
-struct keyData
-{
+struct keyData{
 	float keyF;
 	float keyD;
 }mk;
 
-void play(float key,float del)
-{
+void play(float key,float del){
      sound(key);
      delay(del);
      nosound();
 }
 
-struct node
-{
+struct node{
     struct keyData sf;
     struct node *next;
 	  struct node *prev;
 }*top;
 
-int loadFile(char filename[])
-{
-	int index=0;
+int loadFile(char filename[]){
+    int index=0;
     FILE *ptr=fopen(filename,"rb");
-    if(ptr==NULL)
-    {   
-		printf(" File %s not found",filename);
+    if(ptr==NULL){   
+	printf(" File %s not found",filename);
         return 1;
     }
-    else
-    {
-		    top=NULL;
-        while(!feof(ptr))
-        {
+    else{
+	top=NULL;
+        while(!feof(ptr)){
            fseek(ptr,index*sizeof(mk),SEEK_SET);
            fread(&mk,sizeof(mk),1,ptr);
-		       if(feof(ptr))
-			        break;
+	    if(feof(ptr))
+		break;
            pushToMem(mk.keyF,mk.keyD);
-		       index++;
+	   index++;
         }		
     }
     fclose(ptr);
-	return 0;
+    return 0;
 }
-int pushToMem(float k,float d)
-{
+int pushToMem(float k,float d){
 	struct node *tmpNode;
 	tmpNode=(struct node*)malloc(sizeof(struct node));
 	tmpNode->sf.keyF=k;
 	tmpNode->sf.keyD=d;
 	tmpNode->prev=NULL;
 	if(top==NULL)
-	{
-		tmpNode->next=NULL;
-	}
-	else
-	{
+	   tmpNode->next=NULL;
+	else{
 		tmpNode->next=top;
 		top->prev=tmpNode;
 	}
 	top=tmpNode;
 	return 0;
 }
-int playSong()
-{
+int playSong(){
 	struct node *tmp;
 	tmp=top;
 	while(tmp->next!=NULL)
 		tmp=tmp->next;
-	while(tmp!=NULL)
-	{
+	while(tmp!=NULL){
 		play(tmp->sf.keyF,tmp->sf.keyD);
 		tmp=tmp->prev;
 	}
 	return 0;
 }
 
-int main()
-{
+int main(){
 	loadFile(fnameA);
 	playSong();
 	printf(" Done..");
