c4b03d296f6956ffa49b4c778f2c92d6e914e0fb
Kromi <33200410+4weKromi@users.noreply.github.com>
2018-07-08 14:05:02 +0530

    Update play.c

--- pianoTC/main/play.c
+++ pianoTC/main/play.c
@@ -1,9 +1,7 @@
 /* 
 	Piano modules
 */
-
 #include "GLOBAL.H"
-
 extern char fnameA[];
 extern char fnameB[];
 extern char fnameC[];
@@ -13,8 +11,7 @@ extern char mgList[];
 extern char buff[];
 
 /* MG data structure */
-struct keyData
-{
+struct keyData{
 	float keyF;
 	float keyD;
 }mk2;
@@ -26,16 +23,14 @@ float keyFreq[]={0.0,130.81,138.59,146.83,155.56,164.81,174.61,184.99,195.99,207
 	880};
 
 /* play using frequency and delay*/
-void play(float key,float del)
-{
+void play(float key,float del){
 	 sound(key);
 	 delay(del);
 	 nosound();
 }
 
 /* play first song */
-int startA()
-{
+int startA(){
 	loadFile(fnameA);
 	setcolor(WHITE);
 	rectangle(402,152,478,178);
@@ -48,8 +43,7 @@ int startA()
 }
 
 /* play second song */
-int startB()
-{
+int startB(){
 	loadFile(fnameB);
 	setcolor(WHITE);
 	rectangle(402,202,478,228);
@@ -62,8 +56,7 @@ int startB()
 }
 
 /* play third song */
-int startC()
-{
+int startC(){
 	loadFile(fnameC);
 	setcolor(WHITE);
 	rectangle(402,252,478,278);
@@ -74,9 +67,9 @@ int startC()
 	playSong();
 	return 0;
 }
+
 /* play fourth song */
-int startD()
-{
+int startD(){
 	loadFile(fnameD);
 	setcolor(WHITE);
 	rectangle(402,302,478,328);
@@ -89,8 +82,7 @@ int startD()
 }
 
 /* play fifth song */
-int startE()
-{
+int startE(){
 	loadFile(fnameE);
 	setcolor(WHITE);
 	rectangle(402,352,478,378);
@@ -103,221 +95,185 @@ int startE()
 }
 
 /* Play Piano */
-int runtimePlay()
-{
+int runtimePlay(){
 	int i,ch,j=0;
 	char newF[64];
 	float buffer[4096];
 	setcolor(RED);
 	outtextxy(40,170,"Press [Esc] to stop..");
-	while(ch!=27)
-	{
+	while(ch!=27){
 		setcolor(RED);
 		ch=getch();
-		if(ch==49)
-			{
-				outtextxy(30,315,"C");
-				play(keyFreq[1],200);
-				buffer[j]=keyFreq[1];
-			}
-		else if(ch==50)
-			{
-				outtextxy(45,310,"C#");
-				play(keyFreq[2],200);
-				buffer[j]=keyFreq[2];
-			}
-		else if(ch==51)
-			{
-				outtextxy(60,315,"D");
-				play(keyFreq[3],200);
-				buffer[j]=keyFreq[3];
-				
-			}
-		else if(ch==52)
-			{
-				outtextxy(75,310,"D#");
-				play(keyFreq[4],200);
-				buffer[j]=keyFreq[4];
-			}
-		else if(ch==53)
-			{
-				outtextxy(90,315,"E");
-				play(keyFreq[5],200);
-				buffer[j]=keyFreq[5];
-			}
-		else if(ch==54)
-			{
-				outtextxy(120,315,"F");
-				play(keyFreq[6],200);
-				buffer[j]=keyFreq[6];
-			}
-		else if(ch==55)
-			{
-				outtextxy(135,310,"F#");
-				play(keyFreq[7],200);
-				buffer[j]=keyFreq[7];
-			}
-		else if(ch==56)
-			{
-				outtextxy(150,315,"G");
-				play(keyFreq[8],200);
-				buffer[j]=keyFreq[8];
-			}
-		else if(ch==57)
-			{
-				outtextxy(165,310,"G#");
-				play(keyFreq[9],200);
-				buffer[j]=keyFreq[9];
-			}
-		else if(ch==48)
-			{
-				outtextxy(180,315,"A");
-				play(keyFreq[10],200);
-				buffer[j]=keyFreq[10];
-			}
-		else if(ch==45)
-			{
-				outtextxy(195,310,"A#");
-				play(keyFreq[11],200);
-				buffer[j]=keyFreq[11];
-			}
-		else if(ch==61)
-			{
-				outtextxy(210,315,"B");
-				play(keyFreq[12],200);
-				buffer[j]=keyFreq[12];
-			}
-		else if(ch==113)
-			{
-				outtextxy(240,315,"C");
-				play(keyFreq[13],200);
-				buffer[j]=keyFreq[13];
-			}
-		else if(ch==119)
-			{
-				outtextxy(255,310,"C#");
-				play(keyFreq[14],200);
-				buffer[j]=keyFreq[14];
-			}
-		else if(ch==101)
-			{
-				outtextxy(270,315,"D");
-				play(keyFreq[15],200);
-				buffer[j]=keyFreq[15];
-			}
-		else if(ch==114)
-			{
-				outtextxy(285,310,"D#");
-				play(keyFreq[16],200);
-				buffer[j]=keyFreq[16];
-			}
-		else if(ch==116)
-			{
-				outtextxy(300,315,"E");
-				play(keyFreq[17],200);
-				buffer[j]=keyFreq[17];
-			}
-		else if(ch==121)
-			{
-				outtextxy(330,315,"F");
-				play(keyFreq[18],200);
-				buffer[j]=keyFreq[18];
-			}
-		else if(ch==117)
-			{
-				outtextxy(345,310,"F#");
-				play(keyFreq[19],200);
-				buffer[j]=keyFreq[19];
-			}
-		else if(ch==105)
-			{
-				outtextxy(360,315,"G");
-				play(keyFreq[20],200);
-				buffer[j]=keyFreq[20];
-			}
-		else if(ch==111)
-			{
-				outtextxy(375,310,"G#");
-				play(keyFreq[21],200);
-			}
-		else if(ch==112)
-			{
-				outtextxy(390,315,"A");
-				play(keyFreq[22],200);
-				buffer[j]=keyFreq[22];
-			}
-		else if(ch==91)
-			{
-				outtextxy(405,310,"A#");
-				play(keyFreq[23],200);
-				buffer[j]=keyFreq[23];
-			}
-		else if(ch==93)
-			{
-				outtextxy(420,315,"B");
-				play(keyFreq[24],200);
-				buffer[j]=keyFreq[24];
-			}
-		else if(ch==97)
-			{
-				outtextxy(450,315,"C");
-				play(keyFreq[25],200);
-				buffer[j]=keyFreq[25];
-			}
-		else if(ch==115)
-			{
-				outtextxy(465,310,"C#");
-				play(keyFreq[26],200);
-				buffer[j]=keyFreq[26];
-			}
-		else if(ch==100)
-			{
-				outtextxy(480,315,"D");
-				play(keyFreq[27],200);
-				buffer[j]=keyFreq[27];
-			}
-		else if(ch==102)
-			{
-				outtextxy(495,310,"D#");
-				play(keyFreq[28],200);
-				buffer[j]=keyFreq[28];
-			}
-		else if(ch==103)
-			{
-				outtextxy(510,315,"E");
-				play(keyFreq[29],200);
-				buffer[j]=keyFreq[29];
-			}
-		else if(ch==104)
-			{
-				outtextxy(540,315,"F");
-				play(keyFreq[30],200);
-				buffer[j]=keyFreq[30];
-			}
-		else if(ch==106)
-			{
-				outtextxy(555,310,"F#");
-				play(keyFreq[31],200);
-				buffer[j]=keyFreq[31];
-			}
-		else if(ch==107)
-			{
-				outtextxy(570,315,"G");
-				play(keyFreq[32],200);
-				buffer[j]=keyFreq[32];
-			}
-		else if(ch==108)
-			{
-				outtextxy(585,310,"G#");
-				play(keyFreq[33],200);
-				buffer[j]=keyFreq[33];
-			}
-		else if(ch==59)
-			{
-				outtextxy(600,315,"A");
-				play(keyFreq[34],200);
-				buffer[j]=keyFreq[34];
-			}
+		if(ch==49){
+			outtextxy(30,315,"C");
+			play(keyFreq[1],200);
+			buffer[j]=keyFreq[1];
+		}
+		else if(ch==50){
+			outtextxy(45,310,"C#");
+			play(keyFreq[2],200);
+			buffer[j]=keyFreq[2];
+		}
+		else if(ch==51){
+			outtextxy(60,315,"D");
+			play(keyFreq[3],200);
+			buffer[j]=keyFreq[3];	
+		}
+		else if(ch==52){
+			outtextxy(75,310,"D#");
+			play(keyFreq[4],200);
+			buffer[j]=keyFreq[4];
+		}
+		else if(ch==53){
+			outtextxy(90,315,"E");
+			play(keyFreq[5],200);
+			buffer[j]=keyFreq[5];
+		}
+		else if(ch==54){
+			outtextxy(120,315,"F");
+			play(keyFreq[6],200);
+			buffer[j]=keyFreq[6];
+		}
+		else if(ch==55){
+			outtextxy(135,310,"F#");
+			play(keyFreq[7],200);
+			buffer[j]=keyFreq[7];
+		}
+		else if(ch==56){
+			outtextxy(150,315,"G");
+			play(keyFreq[8],200);
+			buffer[j]=keyFreq[8];
+		}
+		else if(ch==57){
+			outtextxy(165,310,"G#");
+			play(keyFreq[9],200);
+			buffer[j]=keyFreq[9];
+		}
+		else if(ch==48){
+			outtextxy(180,315,"A");
+			play(keyFreq[10],200);
+			buffer[j]=keyFreq[10];
+		}
+		else if(ch==45){
+			outtextxy(195,310,"A#");
+			play(keyFreq[11],200);
+			buffer[j]=keyFreq[11];
+		}
+		else if(ch==61){
+			outtextxy(210,315,"B");
+			play(keyFreq[12],200);
+			buffer[j]=keyFreq[12];
+		}
+		else if(ch==113){
+			outtextxy(240,315,"C");
+			play(keyFreq[13],200);
+			buffer[j]=keyFreq[13];
+		}
+		else if(ch==119){
+			outtextxy(255,310,"C#");
+			play(keyFreq[14],200);
+			buffer[j]=keyFreq[14];
+		}
+		else if(ch==101){
+			outtextxy(270,315,"D");
+			play(keyFreq[15],200);
+			buffer[j]=keyFreq[15];
+		}
+		else if(ch==114){
+			outtextxy(285,310,"D#");
+			play(keyFreq[16],200);
+			buffer[j]=keyFreq[16];
+		}
+		else if(ch==116){
+			outtextxy(300,315,"E");
+			play(keyFreq[17],200);
+			buffer[j]=keyFreq[17];
+		}
+		else if(ch==121){
+			outtextxy(330,315,"F");
+			play(keyFreq[18],200);
+			buffer[j]=keyFreq[18];
+		}
+		else if(ch==117){
+			outtextxy(345,310,"F#");
+			play(keyFreq[19],200);
+			buffer[j]=keyFreq[19];
+		}
+		else if(ch==105){
+			outtextxy(360,315,"G");
+			play(keyFreq[20],200);
+			buffer[j]=keyFreq[20];
+		}
+		else if(ch==111){
+			outtextxy(375,310,"G#");
+			play(keyFreq[21],200);
+			buffer[j]=keyFreq[21];
+		}
+		else if(ch==112){
+			outtextxy(390,315,"A");
+			play(keyFreq[22],200);
+			buffer[j]=keyFreq[22];
+		}
+		else if(ch==91){
+			outtextxy(405,310,"A#");
+			play(keyFreq[23],200);
+			buffer[j]=keyFreq[23];
+		}
+		else if(ch==93){
+			outtextxy(420,315,"B");
+			play(keyFreq[24],200);
+			buffer[j]=keyFreq[24];
+		}
+		else if(ch==97){
+			outtextxy(450,315,"C");
+			play(keyFreq[25],200);
+			buffer[j]=keyFreq[25];
+		}
+		else if(ch==115){
+			outtextxy(465,310,"C#");
+			play(keyFreq[26],200);
+			buffer[j]=keyFreq[26];
+		}
+		else if(ch==100){
+			outtextxy(480,315,"D");
+			play(keyFreq[27],200);
+			buffer[j]=keyFreq[27];
+		}
+		else if(ch==102){
+			outtextxy(495,310,"D#");
+			play(keyFreq[28],200);
+			buffer[j]=keyFreq[28];
+		}
+		else if(ch==103){
+			outtextxy(510,315,"E");
+			play(keyFreq[29],200);
+			buffer[j]=keyFreq[29];
+		}
+		else if(ch==104){
+			outtextxy(540,315,"F");
+			play(keyFreq[30],200);
+			buffer[j]=keyFreq[30];
+		}
+		else if(ch==106){
+			outtextxy(555,310,"F#");
+			play(keyFreq[31],200);
+			buffer[j]=keyFreq[31];
+		}
+		else if(ch==107){
+			outtextxy(570,315,"G");
+			play(keyFreq[32],200);
+			buffer[j]=keyFreq[32];
+		}
+		else if(ch==108){
+			outtextxy(585,310,"G#");
+			play(keyFreq[33],200);
+			buffer[j]=keyFreq[33];
+		}
+		else if(ch==59){
+			outtextxy(600,315,"A");
+			play(keyFreq[34],200);
+			buffer[j]=keyFreq[34];
+		}
 		setfillstyle(SOLID_FILL,BROWN);
 		bar(22,305,610,323);
 		j++;
@@ -337,8 +293,7 @@ int runtimePlay()
 	bar(220,208,280,228);
 	outtextxy(230,215,"Y E S");
 	i=dialogMouse();
-	if(i==5)
-	{
+	if(i==5){
 		FILE *ptr;
 		FILE *fp;
 		char _s[1];
@@ -347,19 +302,16 @@ int runtimePlay()
 			buff[j]='\0';
 		j=0;
 		outtextxy(240,170,"Enter Filename : ");
-		while(ch!=13)
-		{
+		while(ch!=13){
 			ch=getch();
 			sprintf(_s,"%c",ch);
 			outtextxy(220+i,200,_s);
 			i+=10;
-			if((ch>44&&ch<58)||(ch>64&&ch<122))
-			{
+			if((ch>44&&ch<58)||(ch>64&&ch<122)){
 				buff[j]=(char)ch;
 				j++;
 			}
-			if(ch==8)
-			{
+			if(ch==8){
 				dialogBox();
 				for(j=0;buff[j]!='\0';j++)
 					buff[j]='\0';
@@ -377,16 +329,14 @@ int runtimePlay()
 		sprintf(newF,"%s.mg",buff);
 		remove(newF);
 		ptr=fopen(newF,"ab");
-		if(ptr==NULL)
-		{
+		if(ptr==NULL){
 			dialogBox();
 			outtextxy(220,180,"Error Saving");
 			outtextxy(220,200,newF);
 			getch();
 			startPiano();
 		}
-		while(buffer[j]!='\0')
-		{
+		while(buffer[j]!='\0'){
 			mk2.keyF=buffer[j];
 			mk2.keyD=180;
 			fwrite(&mk2,sizeof(mk2),1,ptr);
@@ -397,17 +347,11 @@ int runtimePlay()
 		outtextxy(240,190,"Save to.. 1/2/3 : ");
 		ch=getch();
 		if(ch==49)
-		{
 			deleteC();
-		}
 		else if(ch==50)
-		{
 			deleteD();
-		}
 		else
-		{
 			deleteE();
-		}
 		fp=fopen(mgList,"a");
 		fprintf(fp,">%s;",newF);
 		fclose(fp);
@@ -417,8 +361,7 @@ int runtimePlay()
 		getch();
 		startPiano();
 	}
-	else
-	{
+	else{
 		for(j=0;buffer[j]!='\0';j++)
 			buffer[j]='\0';
 		startPiano();
@@ -427,8 +370,7 @@ int runtimePlay()
 }
 
 /* Play Page */
-int startPiano()
-{
+int startPiano(){
 	cleardevice();
 	drawMenuTab();
 	drawFirstTab();
@@ -438,9 +380,9 @@ int startPiano()
 	runtimePlay();
 	return 0;
 }
+
 /* open page*/
-int openTab()
-{
+int openTab(){
 	cleardevice();
 	drawMenuTab();
 	drawSecondTab();
@@ -450,8 +392,7 @@ int openTab()
 }
 
 /* About Page*/
-int aboutTab()
-{
+int aboutTab(){
 	cleardevice();
 	drawMenuTab();
 	drawThirdTab();
@@ -460,8 +401,7 @@ int aboutTab()
 }
 
 /* quitInterface for Page*/
-int quitTab()
-{
+int quitTab(){
 	quitDialog();
 	funcMouse(dialogMouse());
 	return 0;
